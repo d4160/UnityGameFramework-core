f7b5d72d3d1d3df6e2da27b0a7cd68bb3a9d7401
Diego Fernandez <ntkodaigo@outlook.com>
2024-05-26 15:41:30 -0500

    [New] UIStateMachine

---
+++ Runtime/UI/MonoBehaviours/UIState.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
+public class UIState : MonoBehaviour
+{
+    [SerializeField] protected bool _lockDisable = false;
+    [SerializeField] protected UIStateMachine _stateMachine;
+
+#if ODIN_INSPECTOR
+    [ValueDropdown("_StatesList")]
+#endif
+    [SerializeField] protected UIState[] _prevStates;
+
+#if ODIN_INSPECTOR
+    [ValueDropdown("_StatesList")]
+#endif
+    [SerializeField] protected UIState[] _nextStates;
+
+    [SerializeField] protected bool _forceDisable = false;
+
+    private UIState[] _StatesList => _stateMachine.States;
+
+    public bool LockDisable => _lockDisable;
+
+    public UIStateMachine StateMachine { get => _stateMachine; internal set => _stateMachine = value; }
+
+#if ODIN_INSPECTOR
+    [Button]
+#endif
+    public void ShowState()
+    {
+        _stateMachine.SetActiveState(this, _forceDisable);
+    }
+
+
+#if ODIN_INSPECTOR
+    [Button]
+#endif
+    public void HideState()
+    {
+        _stateMachine.RemoveActiveState(this, _forceDisable);
+    }
+
+#if ODIN_INSPECTOR
+    [Button]
+#endif
+    public void GoNextState()
+    {
+        if (_nextStates == null || _nextStates.Length == 0) return;
+
+        _stateMachine.SetActiveStates(_nextStates, _forceDisable);
+    }
+
+#if ODIN_INSPECTOR
+    [Button]
+#endif
+    public void GoPrevState()
+    {
+        if (_prevStates == null || _prevStates.Length == 0) return;
+
+        _stateMachine.SetActiveStates(_prevStates, _forceDisable);
+    }
+}
